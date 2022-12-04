9686b47bb9ab8aebbe5df05e0e8c2ea3275a5702
MaxTheSyntax <maks@x-net.pl>
2022-12-04 15:15:31 +0100

    Can now print time

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,19 @@
 use std::io; // Input manager
-use std::time;
+use std::time::{Duration, SystemTime};
+use std::thread::sleep; 
+use chrono::{Datelike, Timelike, Local};
 
 fn main() {
-    println!("Hello, world!");
+    let now = Local::now();
+
+    println!(
+        "The current local time is {:02}:{:02}:{:02}, {:02}.{:02}.{} on a {}.",
+        now.hour(),
+        now.minute(),
+        now.second(),
+        now.day(),
+        now.month(),
+        now.year(),
+        now.weekday()
+    );
 }
