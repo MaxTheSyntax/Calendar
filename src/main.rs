54b47354add6984581a5b9595f95fcf7b0c57f32
MaxTheSyntax <maks@x-net.pl>
2022-12-28 15:38:11 +0100

    imported events.json

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,44 @@
 use std::io; // Input manager
-use std::time::{Duration, SystemTime};
-use std::thread::sleep; 
+use std::fs; // Enables modifying
+// use std::thread::sleep;
 use chrono::{Datelike, Timelike, Local};
 
 fn main() {
-    let now = Local::now();
+    let current_time = Local::now();
+    let events_file_path = "/home/maxthesyntax/code/rust/Calendar/src/events.json";
+    let events = fs::read_to_string(events_file_path)
+        .expect("Unable to read file");
+
 
     println!(
-        "The current local time is {:02}:{:02}:{:02}, {:02}.{:02}.{} on a {}.",
-        now.hour(),
-        now.minute(),
-        now.second(),
-        now.day(),
-        now.month(),
-        now.year(),
-        now.weekday()
+        "Today is {:02}:{:02}:{:02}, {:02}.{:02}.{} on a {}.",
+        current_time.hour(),
+        current_time.minute(),
+        current_time.second(),
+        current_time.day(),
+        current_time.month(),
+        current_time.year(),
+        current_time.weekday()
     );
+}
+
+fn data() {
+    let parsed = json::parse(r#"
+    {
+        "name": "John Doe",
+        "age": 43,
+        "address": {
+            "street": "10 Downing Street",
+            "city": "London"
+        },
+        "phones": [
+            "+44 1234567",
+            "+44 2345678"
+        ]
+    }
+    "#).unwrap();
+
+    println!("{:#}", parsed["name"] );
+
+
 }
