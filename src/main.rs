7da9ac4c394a285e8bfb1dcdd6a23927dfc1a972
MaxTheSyntax <maks@x-net.pl>
2022-12-30 00:00:15 +0100

    can now get amount of days in month

--- src/main.rs
+++ src/main.rs
@@ -4,41 +4,32 @@ use std::fs; // Enables modifying
 use chrono::{Datelike, Timelike, Local};
 
 fn main() {
-    let current_time = Local::now();
     let events_file_path = "/home/maxthesyntax/code/rust/Calendar/src/events.json";
     let events = fs::read_to_string(events_file_path)
         .expect("Unable to read file");
+    let time = Local::now();
+    let month_names: [&str; 12] = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let month_name = (time.month() - 1) as usize;   
+    let days_in_month = match time.month() {
+        1 | 3 | 5 | 7 | 8 | 10 | 12 => 31,
+        _ => 30,
+    }; 
 
+    print_date_and_time();
+    println!("   {}", month_names[month_name]);
+}
+
+fn print_date_and_time() {
+    let time = Local::now();
 
     println!(
         "Today is {:02}:{:02}:{:02}, {:02}.{:02}.{} on a {}.",
-        current_time.hour(),
-        current_time.minute(),
-        current_time.second(),
-        current_time.day(),
-        current_time.month(),
-        current_time.year(),
-        current_time.weekday()
+        time.hour(),
+        time.minute(),
+        time.second(),
+        time.day(),
+        time.month(),
+        time.year(),
+        time.weekday()
     );
-}
-
-fn data() {
-    let parsed = json::parse(r#"
-    {
-        "name": "John Doe",
-        "age": 43,
-        "address": {
-            "street": "10 Downing Street",
-            "city": "London"
-        },
-        "phones": [
-            "+44 1234567",
-            "+44 2345678"
-        ]
-    }
-    "#).unwrap();
-
-    println!("{:#}", parsed["name"] );
-
-
 }
