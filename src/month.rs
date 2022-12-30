f64a5a23911747d04296413899961afc944e5450
MaxTheSyntax <maks@x-net.pl>
2022-12-30 20:02:38 +0100

    optimized with dads help :)

---
+++ src/month.rs
@@ -0,0 +1,40 @@
+use std::fs; // Enables modifying file system       
+use std::str;
+// use std::thread::sleep;
+use chrono::{Datelike, Timelike, Local};
+use colored::Colorize;
+
+pub fn main() {
+    let events_file_path = "/home/maxthesyntax/code/rust/Calendar/src/events.json";
+    let events = fs::read_to_string(events_file_path)
+        .expect("Unable to read file");
+    let time = Local::now();
+    let month_names: [&str; 12] = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let month_name = (time.month() - 1) as usize;   
+    let days_in_month = match time.month() {
+        1 | 3 | 5 | 7 | 8 | 10 | 12 => 31,
+        2 => 28,
+        _ => 30,
+    }; 
+
+    println!("   {}", month_names[month_name]);
+    let mut days_to_print_in_ln = 7;
+    let mut current_day = 1;
+    while days_in_month as usize >= current_day {
+        if days_to_print_in_ln == 0 {
+            println!("");
+            days_to_print_in_ln = 7;
+        }
+            else {
+                if current_day == time.day() as usize {
+                    print!("{:>2} ", current_day.to_string().green().bold());
+                }
+                    else {
+                        print!("{:>2} ", current_day);
+                    }
+                days_to_print_in_ln = days_to_print_in_ln - 1;
+                current_day = current_day + 1;
+            }
+    }
+    println!("");
+}
